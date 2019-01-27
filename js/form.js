e83b8df22426bc01245a290f78753663bb356042
kingeugene <king.eugene1503@gmail.com>
2019-01-27 23:01:45 +0200

    34

--- js/form.js
+++ js/form.js
@@ -39,11 +39,11 @@ $(document).ready(function() {
 	  data: data,
 	  success: function( response ) {
 	  	if ( response.status == 'Error') {
-	  		errortAlert.text('response.message').show(); 
+	  		errortAlert.text( response.message ).show(); 
 	  	}else if ( response.status == 'OK') {
 	  		location.href = 'index.html'; 
 	  	}else if ( response.status == 'Form Error' ) {
-	  		errortAlert.text('response.message').show(); 
+	  		errortAlert.text( response.message ).show(); 
 	  	}else {}
 	  	
 	  },
