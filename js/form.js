70730e01e792da57bd4655afbf1a3af3f6913b2b
kingeugene <king.eugene1503@gmail.com>
2019-01-27 20:50:10 +0200

    1

---
+++ js/form.js
@@ -0,0 +1,24 @@
+use strict
+$(document).ready(function() {
+	('.send_btn').on('click', function( event ) {
+		event.preventDefault();
+		const form = $('#form');
+		if ( form.find( 'input[name="mf_name"]' ).val() == '' ) {
+			alert('Ты тупой');
+	}
+		if ( form.find( 'input[name="mf_name"]' )) {
+			
+	}
+		if ( form.find( 'input[name="mf_name"]' )) {
+			
+	}
+		if ( form.find( 'input[name="mf_name"]' )) {
+			
+	}
+		if ( form.find( 'input[name="mf_name"]' )) {
+			
+	}
+		if ( form.find( 'input[name="mf_name"]' )) {
+			
+	}
+});
