b22c66dc952e3e3c56fd06297fb31d8246cee128
kingeugene <king.eugene1503@gmail.com>
2019-01-29 22:53:54 +0200

    endCommit

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 	let companies = null,
 	company = null;
 
+	//Getting list of companies
 	$.ajax({
 	  type: 'GET',
 	  url: 'http://codeit.pro/codeitCandidates/serverFrontendTest/company/getList',
@@ -20,23 +21,26 @@ $(document).ready(function() {
 	  success: function( response ) {
 	  	companies = response.list;
 	  	$(total_companies).find('.itemComp-main').text(response.list.length);
+	  	//Iterating the array to append all companies to the block
 	  	response.list.forEach(function(el){
 	  		$(list_companies).find('.itemComp-list__table').append(`
 	  			<tr><td class="company-item" data-name="`+el.name+`">`+el.name+`</td></tr>
 	  			`);
 	  	});
-	  	console.log(JSON.stringify(companies));
 	  },
 	  complete: function(){
 	  	$(list_companies).find('.itemComp-list__table').removeClass('loader');
 	  	$(total_companies).find('.itemComp-main').removeClass('loader');
 	  	$(locationCompanies).find('.itemComp-main').removeClass('loader');
 
+	  	//Addinf action on items that has been loaded
 		$('.company-item').on('click', function(){
 			partners.find('.itemComp-main__wrap').text('');
 			let $this = $(this);
 			partners.show();
+			partners.find('.itemComp-main__wrap').data('name', $this.data('name'));
 			company = companies.filter((el) => {return el.name === $this.data('name') })[0];
+			company.partners.sort((el1, el2) => {return el1.value < el2.value ? 1 : -1; });
 			company.partners.forEach(function(el){
 				partners.find('.itemComp-main__wrap').append(`
 					<div class="itemComp-listPartn">
@@ -50,6 +54,7 @@ $(document).ready(function() {
 	  }
 	});
 
+	//Getting list of news
 	$.ajax({
 	  type: 'GET',
 	  url: 'http://codeit.pro/codeitCandidates/serverFrontendTest/news/getList',
@@ -59,8 +64,8 @@ $(document).ready(function() {
 	  success: function( response ) {
 	  	let date = null;
 	  	$(news).find('.bxslider').removeClass('loader');
+	  	//Creating block for each news
 	  	response.list.forEach(function(el){
-	  		//alert(el.date);
 	  		date = new Date(el.date);
 	  		$(news).find('.bxslider').append(`
 					<li class="itemComp-news">
@@ -79,6 +84,39 @@ $(document).ready(function() {
 	  	});
 		$(news).find('.bxslider').bxSlider({controls: false});
 	  }
+	});
+
+	//Sort columns by parametrs from tag attributes
+	$('.sort').on('click', function(){
+		const order = $(this).data('order'),
+		value = $(this).data('value'),
+		name = partners.find('.itemComp-main__wrap').data('name');
+		company = companies.filter((el) => {return el.name === name })[0];
+		company.partners.sort((el1, el2) => {
+			if(value === 'name'){
+				if(order === 'asc'){
+					return el1.name < el2.name ? 1 : -1; 
+				} else{
+					return el1.name < el2.name ? -1 : 1; 
+				} 
+			} else{
+				if(order === 'asc'){
+					return el1.value < el2.value ? 1 : -1; 
+				} else{
+					return el1.value < el2.value ? -1 : 1; 
+				} 
+			}
+		});
+		partners.find('.itemComp-main__wrap').text('');
+		company.partners.forEach(function(el){
+				partners.find('.itemComp-main__wrap').append(`
+					<div class="itemComp-listPartn">
+						<div class="itemComp-main">`+el.value+`</div>
+						<div class="blockList">`+el.name+`</div>
+					</div>
+					`);
+			});
+		partners.show();
 	});
 
 });
